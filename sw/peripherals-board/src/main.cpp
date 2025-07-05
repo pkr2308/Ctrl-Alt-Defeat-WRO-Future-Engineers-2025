67c760e0bbc7d296b0c3b96d951a541bd0df9252
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2025-07-05 10:53:46 +0530

    Create files for drivers

--- sw/peripherals-board/src/main.cpp
+++ sw/peripherals-board/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
+#include "SensorManager/SensorManager.h"
+#include "CommManager/CommManager.h"
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+
 }
---
+++ sw/peripherals-board/src/pindefs.h
@@ -0,0 +1,32 @@
+/*
+
+  pindefs
+  Pin definitions for peripherals board
+  See /hw/peripherals-board for schematic
+
+  Written by DIY Labs
+
+*/
+
+#pragma once
+
+#define PIN_RGB_BUILTIN       16
+
+#define PIN_TB6612_PWMB       26
+#define PIN_TB6612_BIN1       28
+#define PIN_TB6612_BIN2       27
+#define PIN_TB6612_STBY       29
+
+#define PIN_STEERING_SERVO    4
+
+#define PIN_MOTOR_ENCA        2
+#define PIN_MOTOR_ENCB        3
+
+#define PIN_RX_CH1            15
+#define PIN_RX_CH2            14
+
+#define PIN_I2C0_SCL          1
+#define PIN_I2C0_SDA          0
+#define PIN_I2C1_SCL          11
+#define PN_I2C1_SDA           10
+
