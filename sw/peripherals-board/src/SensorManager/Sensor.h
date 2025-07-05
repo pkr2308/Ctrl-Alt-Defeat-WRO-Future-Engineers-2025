7c6bf9b6a1ed4c6013e25151c5c0f06c2d4a3ff3
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2025-07-05 18:10:57 +0530

    Create base class for sensor drivers

---
+++ sw/peripherals-board/src/SensorManager/Sensor.h
@@ -0,0 +1,41 @@
+/*
+
+  Sensor.h
+  Defines base class for sensors managed by SensorManager.
+  Sensor drivers must inherit from this class.
+
+  Written by DIY Labs
+
+*/
+
+#pragma once
+
+#include <Adafruit_Sensor.h>
+#include <vector>
+
+/**
+ * @brief Base class for sensors managed by SensorManager. Sensor drivers must inherit from this class.
+ */
+class Sensor{
+
+  public:
+
+  /**
+   * @brief Default destructor for Sensor class.
+   */
+  virtual ~Sensor() = default;
+
+  /**
+   * @brief Initializes sensor.
+   * @return True if initialization is successful, false otherwise.
+   */
+  virtual bool initialize() = 0;
+
+  /**
+   * @brief Updates sensor data.
+   * @return Vector containing sensor events.
+   * 
+   */
+  virtual std::vector<sensors_event_t> update() = 0;
+
+};
