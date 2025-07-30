d88e3f69fee509d2cee0cb7ddff94042245f0f8c
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2025-07-30 06:27:13 +0530

    Implement basic target control driver

---
+++ sw/peripherals-board/src/drivers/hwrev1_target_control.cpp
@@ -0,0 +1,28 @@
+#include <hwrev1_target_control.hpp>
+
+hw_rev_1_TargetControl::hw_rev_1_TargetControl(VehicleConfig cfg){
+
+  _config =cfg;
+
+}
+
+void hw_rev_1_TargetControl::init(IMotorDriver* motorDriver, ISteeringDriver* steeringDriver){
+
+  _motorDriver = motorDriver;
+  _steeringDriver = steeringDriver;
+
+  // TODO: implement nullptr check and log
+
+  _motorDriver->init();
+  _steeringDriver->init();
+
+}
+
+void hw_rev_1_TargetControl::targetControl(VehicleCommand cmd){
+
+  // implement PID
+
+  _steeringDriver->steer(cmd.targetYaw);
+  _motorDriver->driveMotor(cmd.targetSpeed, true);
+
+}
