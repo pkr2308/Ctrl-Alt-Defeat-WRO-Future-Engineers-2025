2dc47205a6df2450960f55ee25b8dcab6e2b8655
Pranav Kiran Rajarathna <pranavkr.2308@gmail.com>
2025-08-12 15:33:26 +0530

    Make in-run changes to target yaw and distance values

--- sw/peripherals-board/src/drivers/hwrev2/hwrev2_single_lidar_open_round.cpp
+++ sw/peripherals-board/src/drivers/hwrev2/hwrev2_single_lidar_open_round.cpp
@@ -16,7 +16,7 @@ void hw_rev_2_SingleLidarOpenRound::init(ILogger* logger) {
   _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::init()", _debugLogger->INFO, "Initialising drive algorithm");
 
   if (front_startDist > 120) stopDist = 0;
-  else stopDist = 20;
+  else stopDist = 30;
   
   speed = 225;                      // Initial speed
   VehicleCommand{.targetSpeed = speed, .targetYaw = 90}; // Set initial speed, steering
@@ -35,18 +35,20 @@ VehicleCommand hw_rev_2_SingleLidarOpenRound::drive(VehicleData vehicleData){
   if (difference > 180) difference = 360 - difference;
   else if (difference < -180) difference = 360 + difference;
   if (turning == true){
-    if (abs(difference) <= 4.5){   // Return to straight after turning for ~85°
+    if (abs(difference) <= 3){   // Return to straight after turning for ~85°
         speed = 225;
         turning = false;
         encoderValue = 0;
         distance = 0;
         turns += 1;
+        targetYaw = targetYaw - turns*0.5; // Adjust targetYaw for next turn
+        threshold -= 1.2; // Decrease threshold for next turn, there are drifting tendencies; Ref Logs
         pos = 90; // Reset servo position
         command.targetYaw = pos;
         _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::drive()", _debugLogger->INFO, "Stopping turn TY:" + String(targetYaw) + " deg Yaw" + String(yaw) + " deg");   
     }
-    else if (abs(difference) > 15 && abs(difference) < 85){ // Continue turning
-        pos = 90 + turnDir * (47 - (90-abs(difference))/3.3); // Set servo position for turning
+    else if (abs(difference) > 8 && abs(difference) < 78){ // Continue turning
+        pos = 90 + turnDir * (47 - (90-abs(difference))/3); // Set servo position for turning
         _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::drive()", _debugLogger->INFO, "steering" + String(pos) + " " + String(yaw));
     }
   }
@@ -58,6 +60,7 @@ VehicleCommand hw_rev_2_SingleLidarOpenRound::drive(VehicleData vehicleData){
 
   if ((front_lidarDist < threshold) and (turning == false) and (turns == 0 or distance > 115) and (abs(difference) < 5) and (left_lidarDist + right_lidarDist > 120)){ // Checking to turn
     speed = 190;
+    if (turns == 11) speed = 180; // Slow down for final turn
     turning = true;
     pos = 90 + turnDir * 47; // Set servo position for turning
     targetYaw = yaw + turnDir * 90;
@@ -68,14 +71,6 @@ VehicleCommand hw_rev_2_SingleLidarOpenRound::drive(VehicleData vehicleData){
     else if (targetYaw > 345 or targetYaw < 15) targetYaw = 0;
     _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::drive()", _debugLogger->INFO, "Start turn" + String(targetYaw) + " deg" + String(front_lidarDist) + " cm");
 
-  }
-  
-  // Stop after 3 rounds
-  if (turns == 12 && distance >= stopDist){
-    completed = true;
-    speed = 0; // Stop the vehicle
-    pos = 90; // Reset servo position
-    _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::drive()", _debugLogger->INFO, "Completed 3 rounds");
   }
 
   // Not turning - Gyro straight follower
@@ -104,8 +99,15 @@ VehicleCommand hw_rev_2_SingleLidarOpenRound::drive(VehicleData vehicleData){
 
   //if ((turnDir == 0) && (left_lidarDist > left_startDist + 60)) turnDir = -1; // Turning to left
   //else if ((turnDir == 0) && (right_lidarDist > right_startDist + 60)) turnDir = 1; // Turning to right
-  if (turns == 12) speed = 190;
-
+  if (turns == 12){
+    if (distance >= stopDist){
+        completed = true;
+        speed = 0; // Stop the vehicle
+        pos = 90; // Reset servo position
+        _debugLogger->sendMessage("hw_rev_2_SingleLidarOpenRound::drive()", _debugLogger->INFO, "Completed 3 rounds");
+    }
+    else speed = 190; // Slow down before stopping
+  }
   command.targetSpeed = speed;
   // This sets servo position not yaw since this system is currently on direct control
   // Ref .hpp file with line 19 - bool isDirectControl() override { return true; }
