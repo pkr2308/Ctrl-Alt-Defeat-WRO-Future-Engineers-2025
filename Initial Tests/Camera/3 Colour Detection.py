9d355d1521ca9f4607c6766f79c7a54f3643ce08
Pranav Kiran Rajarathna <pranavkr.2308@gmail.com>
2025-08-17 23:24:16 +0530

    Update opencv tests to work with PiCamera 3 (NoIR)

--- Initial Tests/Camera/3 Colour Detection.py
+++ Initial Tests/Camera/3 Colour Detection.py
@@ -1,22 +1,29 @@
 import cv2
 import numpy as np
+from picamera2 import Picamera2
+import time
 
-# Start video capture
-cap = cv2.VideoCapture(0)
+# For standard camera use "imx219.json"
+tuning = Picamera2.load_tuning_file("imx219_noir.json")
+picam2 = Picamera2(tuning = tuning)
+
+picam2.start_preview()
+time.sleep(2)  # Let the camera warm up
+
+picam2.start()
 
 while True:
-    # Read each frame from the webcam
-    ret, frame = cap.read()
-
-    if not ret:
-        break
+    
+    frame = picam2.capture_array()
+    corrected_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Convert the frame to HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    hsv = cv2.cvtColor(corrected_frame, cv2.COLOR_RGB2HSV)
 
     # Define the range for white color in HSV space
-    lower_white = np.array([130, 100, 110])
-    upper_white = np.array([200, 170, 180])
+    lower_white = np.array([0, 0, 150])
+    upper_white = np.array([179, 30, 255])
+    # Hue for white specifically is from 0-179, since it is achromatic
 
     # Create a mask to detect white color
     mask1 = cv2.inRange(hsv, lower_white, upper_white)
@@ -26,16 +33,16 @@ while True:
     upper_white2 = np.array([255, 255, 255])
 
     # Create another mask and combine it with the first one
-    mask2 = cv2.inRange(hsv, lower_white2, upper_white2)
+    #mask2 = cv2.inRange(hsv, lower_white2, upper_white2)
 
     # Combine both masks to get a better detection
-    mask = mask1 + mask2
+    mask = mask1 #+ mask2
 
     # Apply the mask on the original image
-    result = cv2.bitwise_and(frame, frame, mask=mask)
+    result = cv2.bitwise_and(corrected_frame, corrected_frame, mask=mask)
 
     # Display the original frame and the result
-    cv2.imshow("Original", frame)
+    cv2.imshow("Original", corrected_frame)
     cv2.imshow("white Object Detection", result)
 
     # Press 'q' to exit the loop
@@ -43,5 +50,5 @@ while True:
         break
 
 # Release the capture and close windows
-cap.release()
+picam2.stop()
 cv2.destroyAllWindows()
