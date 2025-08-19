3979f5007851c01015aa08b03ffff4574e5fb186
Pranav Kiran Rajarathna <pranavkr.2308@gmail.com>
2025-08-19 15:41:19 +0530

    Update black colour ranges to detect walls

--- Initial Tests/Camera/3 Colour Detection.py
+++ Initial Tests/Camera/3 Colour Detection.py
@@ -52,13 +52,18 @@ while True:
     lower_red = np.array([0, 120, 88])
     upper_red = np.array([25, 255, 255])
     lower_green = np.array([52, 120, 78])
-    upper_green = np.array([67, 255, 255])
+    upper_green = np.array([70, 255, 255])
+    lower1_black = np.array([37, 65, 20])
+    upper1_black = np.array([65, 130, 60])
+    lower2_black = np.array([40, 130, 50])
+    upper2_black = np.array([49, 175, 90])
     # The 'magenta' parking pieces also show up as red!
 
     # Create a mask to detect colour
     mask_red = cv2.inRange(hsv_frame, lower_red, upper_red)
     mask_green = cv2.inRange(hsv_frame, lower_green, upper_green)
-
+    mask_black = cv2.inRange(hsv_frame, lower1_black, upper1_black) + cv2.inRange(hsv_frame, lower2_black, upper2_black)
+    
     # Create another mask and combine it with the first one
 
 
@@ -70,7 +75,8 @@ while True:
 
     # Display the original frame and the result
     cv2.imshow("Original", corrected_frame)
-    cv2.imshow("Colour Detection", result)
+    cv2.imshow("Black Mask", mask_black)
+    cv2.imshow("Colours", result)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
