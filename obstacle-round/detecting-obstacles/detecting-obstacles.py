7dcbe8d4eafa552d7e6d3005f16ad3743cdbb713
Pranav Kiran Rajarathna <pranavkr.2308@gmail.com>
2025-08-21 13:59:37 +0530

    Detect the nearest obstacle colour and position

--- obstacle-round/detecting-obstacles/detecting-obstacles.py
+++ obstacle-round/detecting-obstacles/detecting-obstacles.py
@@ -1,9 +1,10 @@
-from picamera2 import Picamera2
-import numpy as np
 import cv2
+import numpy as np
+from picamera2 import Picamera2
 import time
 
-tuning = Picamera2.load_tuning_file("imx219_noir.json")
+# For standard camera use "imx219.json"
+tuning = Picamera2.load_tuning_file("imx219.json")
 picam2 = Picamera2(tuning = tuning)
 
 config = picam2.create_video_configuration(main={"size": (1280, 720)})
@@ -11,82 +12,98 @@ picam2.configure(config)
 
 picam2.start_preview()
 time.sleep(2)  # Let the camera warm up
+
 picam2.start()
 
 def init():
     global lower_red, upper_red, lower_green, upper_green, lower1_black, upper1_black, lower2_black, upper2_black
+    global red_obs, green_obs
+    
+    #Define colour ranges
     lower_red = np.array([0, 120, 88])
-    upper_red = np.array([25, 255, 255])
+    upper_red = np.array([19, 255, 255])
     lower_green = np.array([52, 120, 78])
     upper_green = np.array([70, 255, 255])
     lower1_black = np.array([37, 65, 20])
     upper1_black = np.array([65, 130, 60])
     lower2_black = np.array([40, 130, 50])
     upper2_black = np.array([49, 175, 90])
-    # The 'magenta' parking pieces also show up as red!  
+    # The 'magenta' parking pieces also show up as red!
+
+    red_obs = []
+    green_obs = []  
 
 def process_frame():
-    global masked_img, hsv_frame
+    global hsv_frame, red_obs, green_obs, hsv_roi, corrected_frame
     # Create a mask to detect colour
-    mask_red = cv2.inRange(hsv_frame, lower_red, upper_red)
-    mask_green = cv2.inRange(hsv_frame, lower_green, upper_green)
-    mask_black = cv2.inRange(hsv_frame, lower1_black, upper1_black) + cv2.inRange(hsv_frame, lower2_black, upper2_black)
-
-    mask = mask_red + mask_green
-
-    # Apply the mask on the original image
-    masked_img = cv2.bitwise_and(frame, frame, mask=mask)
+    mask_red = cv2.inRange(hsv_roi, lower_red, upper_red)
+    mask_green = cv2.inRange(hsv_roi, lower_green, upper_green)
+    mask_black = cv2.inRange(hsv_roi, lower1_black, upper1_black) + cv2.inRange(hsv_roi, lower2_black, upper2_black)
     
     # Find contours for red,green and black. We don't want the hierarchy
     red_contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     green_contours, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     black_contours, _ = cv2.findContours(mask_black, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(corrected_frame, red_contours, -1, (255,100,100), -1)
-    cv2.drawContours(corrected_frame, red_contours, -1, (100,255,100), -1)
-    cv2.drawContours(corrected_frame, red_contours, -1, (120,120,120), -1)
-    
+    # Contours may be drawn
+    #for contour in red_contours: cv2.drawContours(corrected_frame, [contour + (0,350)], -1, (100,100,255), -1)
+    #for contour in green_contours: cv2.drawContours(corrected_frame, [contour + (0,350)], -1, (100,255,100), -1)
+    #for contour in black_contours: cv2.drawContours(corrected_frame, [contour + (0,350)], -1, (130,130,130), -1)
 
     # Filter and locate obstacle bounding boxes
-    red_obs = get_obstacle_positions(red_contours)
-    green_obs = get_obstacle_positions(green_contours)
+    red_obs = get_obstacle_positions(red_contours, red_obs)
+    green_obs = get_obstacle_positions(green_contours, green_obs)
 
     # Draw contours for visualization
-    for x, y, w, h in red_obs:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
-    for x, y, w, h in green_obs:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    print(f"Red: {red_obs}")
+    print(f"Green: {green_obs}")
+    for item in red_obs:
+        x = item[0][0]
+        y = item[0][1] + 350    #ROI was cropped
+        w = item[0][2]
+        h = item[0][3]
+        cv2.rectangle(corrected_frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    for item in green_obs:
+        x = item[0][0]
+        y = item[0][1] + 350    # ROI was cropped
+        w = item[0][2]
+        h = item[0][3]
+        cv2.rectangle(corrected_frame, (x,y), (x+w,y+h), (0,255,0), 2)
 
-    return frame, red_obs, green_obs
+    return corrected_frame, red_obs, green_obs
 
-def get_obstacle_positions(contours):
+def get_obstacle_positions(contours, obs):
     obs = []
-    min_area = 600  # minimum contour area to be obstacle in pixels
+    min_area = 500  # minimum contour area to be obstacle in pixels
+    max_area = 30000
     print(frame.shape)
-    fh, fw = frame.shape[0], frame.shape[8]
+    fh, fw = frame.shape[0], frame.shape[1]
 
     for cnt in contours:
-        if cv2.contourArea(cnt) > min_area:
+        if cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < max_area:
             x,y,w,h = cv2.boundingRect(cnt)
-            # Determine grid cell (3 rows x 2 columns)
-            row = 0 if y+h/2 < fh/3 else (1 if y+h/2 < 2*fh/3 else 2)
-            col = 0 if x+w/2 < fw/2 else 1
-            obs.append({"box": (x,y,w,h), "grid": (row, col)})
+            if h > w:
+                # Determine grid cell (3 rows x 2 columns)
+                # TODO when driving integration done
+                row = 0
+                col = 0
+                obs.append([(x,y,w,h), (row, col)])
     return obs
 
 
 def decide_path(red_obs, green_obs):
     # Needs to be updated
-    # Grid cell positions occupied by obstacles
-    red_cells = [pos['grid'] for pos in red_obs]
-    green_cells = [pos['grid'] for pos in green_obs]
+    # Grid positions occupied by obstacles
+    red_cells = [pos[1] for pos in red_obs]
+    green_cells = [pos[1] for pos in green_obs]
 
     # Logic:
     # If red obstacle detected as nearest, drive left of it
     # If green obstacle detected as nearest, drive right of it
-    # If two obstacles, they are at ends, so robot drives the middle lane
 
-    # Simplified path selection
+    current_obs = nearest_obstacle()
+    print(f'The current obstacle to tackle is {current_obs}')
+    '''
     if len(red_cells) == 1 and not green_cells:
         # Obstacle red detected: drive left of obstacle means steer left
         action = "STEER_LEFT"
@@ -99,17 +116,31 @@ def decide_path(red_obs, green_obs):
     else:
         # No obstacles detected
         action = "STRAIGHT"
+    '''
+    return 'logic'
 
-    return action
+def nearest_obstacle():
+    global red_obs, green_obs
+    nearest_obs = [(590,0,100,150),'colour']
+    for obs in red_obs: 
+        if nearest_obs[0][1] < obs[0][1]: 
+            nearest_obs = obs
+            nearest_obs[1] = 'red'
+    for obs in green_obs: 
+        if nearest_obs[0][1] < obs[0][1]: 
+            nearest_obs = obs
+            nearest_obs[1] = 'green'
+    return nearest_obs
 
 def run():
-    global frame, hsv_frame, corrected_frame
+    global frame, hsv_frame, corrected_frame, hsv_roi
     while True:
         # Read a frame from the camera
         frame = picam2.capture_array()
         corrected_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Convert the frame to HSV color space
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        hsv_roi = hsv_frame[350:715, 0:1280]        # Our region of interest is only the bottom half of the camera feed
 
         # Process camera frame for obstacles
         frame_processed, red_obs, green_obs = process_frame()
@@ -121,6 +152,7 @@ def run():
         cv2.putText(frame_processed, f"Path: {path_action}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 
         cv2.imshow("Obstacle Detection", frame_processed)
+        #cv2.imshow("Contours", corrected_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
