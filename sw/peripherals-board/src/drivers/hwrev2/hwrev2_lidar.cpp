9e79ea436845f90acea0b246b14f9690bef05425
DIY <onelabtorulethemall@gmail.com>
2025-08-24 05:59:10 +0530

    Add debug messages in LiDAR driver update function

--- sw/peripherals-board/src/drivers/hwrev2/hwrev2_lidar.cpp
+++ sw/peripherals-board/src/drivers/hwrev2/hwrev2_lidar.cpp
@@ -25,11 +25,40 @@ std::vector<SensorData> hw_rev_2_lidar::update(){
   SensorData data;
   data.sensorDataType = SENSOR_LIDAR;
 
-  _lidar->getData(data.lidar[0], _config.addressConfig.frontLidarAddr);
-  _lidar->getData(data.lidar[90], _config.addressConfig.rightLidarAddr);
-  _lidar->getData(data.lidar[180], _config.addressConfig.backLidarAddr);
-  _lidar->getData(data.lidar[270], _config.addressConfig.leftLidarAddr);
+  data.lidar[0] = 0;
+  data.lidar[90] = 0;
+  data.lidar[180] = 0;
+  data.lidar[270] = 0;
 
+  if(!_lidar->getData(data.lidar[0], _config.addressConfig.frontLidarAddr)){
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from front LiDAR returned false");
+  }
+  else{
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from front LiDAR returned true");
+  }
+
+  if(!_lidar->getData(data.lidar[90], _config.addressConfig.rightLidarAddr)){
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from right LiDAR returned false");
+  }
+  else{
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from right LiDAR returned true");
+  }
+
+  if(!_lidar->getData(data.lidar[180], _config.addressConfig.backLidarAddr)){
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from back LiDAR returned false");
+  }
+  else{
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from back LiDAR returned true");
+  }
+
+  if(!_lidar->getData(data.lidar[270], _config.addressConfig.leftLidarAddr)){
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from left LiDAR returned false");
+  }
+  else{
+    _logger->sendMessage("hw_rev_2_lidar::update()", _logger->ERROR, "Data read from left LiDAR returned true");
+  }
+
+  
   dataVector.push_back(data);
 
   return dataVector;
